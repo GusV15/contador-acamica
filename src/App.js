3523301d4f4100bce5209951c9f5b2f30e893490
Gustavo Velasquez <gustavov0706@gmail.com>
2020-06-13 00:30:10 -0300

    state counter component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h2>¡Hola!</h2>
-      <h4>Inserta tu contador aquí.</h4>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    console.log("El componente se ha cargado en el DOM");
+  }
+  componentDidUpdate() {
+    console.log("El componente se ha actualizado");
+  }
+  componentWillUnmount() {
+    console.log("El componente ya no existe");
+  }
+  handleClick = e => {
+    this.setState({
+      count: +e.target.value
+    });
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="App">
+        <h2>{count}</h2>
+        <div className="container">
+          <button className="btn" value={count + 1} onClick={this.handleClick}>
+            Increment
+          </button>
+            <button className="btn" value={count - 1} onClick={this.handleClick}>
+              Decrement
+          </button>
+            <button className="btn" value={0} onClick={this.handleClick}>
+              Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
+
