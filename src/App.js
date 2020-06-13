b1eba978b71538b800a3fe45133e7a2bded99e0e
Gustavo Velasquez <gustavov0706@gmail.com>
2020-06-13 00:05:59 -0300

    start project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>¡Hola!</h2>
+      <h4>Inserta tu contador aquí.</h4>
+    </div>
+  );
+}
